957f5b8876af40dff66b828392546b7eb7e2d1ae
Keenan Johnson <keenan.johnson1@outlook.com>
2022-02-07 10:55:13 -0500

    Add Media interface

---
+++ src/app/interfaces/media.ts
@@ -0,0 +1,7 @@
+export interface Media {
+    main_image?:         string;
+    thumbnail?:          string;
+    virtual_tour_url?:   string;
+    large_image_list:    string[];
+    original_image_list: string[];
+}
