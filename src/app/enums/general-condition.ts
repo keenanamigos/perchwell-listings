a7cde82ce57cf2f425f3536b26e3cb535cda0128
Keenan Johnson <keenan.johnson1@outlook.com>
2022-02-07 10:26:55 -0500

    Add GeneralCondition enum

---
+++ src/app/enums/general-condition.ts
@@ -0,0 +1,12 @@
+export enum GeneralCondition {
+    Empty = "",
+    Estate = "Estate",
+    Excellent = "Excellent",
+    Good = "Good",
+    Mint = "Mint",
+    MintNew = "Mint/New",
+    RawSpace = "Raw Space",
+    TripleMint = "Triple Mint",
+    TrippleMint = "Tripple Mint",
+    VeryGood = "Very Good",
+}
