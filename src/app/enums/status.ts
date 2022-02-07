a3902f8381b29243ea77d4abc59e73e307b38453
Keenan Johnson <keenan.johnson1@outlook.com>
2022-02-07 10:59:46 -0500

    Add Status enum

---
+++ src/app/enums/status.ts
@@ -0,0 +1,7 @@
+export enum Status {
+    Active = "Active",
+    Expired = "Expired",
+    PermanentlyOffMarket = "Permanently Off Market",
+    Rented = "Rented",
+    TemporarilyOffMarket = "Temporarily Off Market",
+}
