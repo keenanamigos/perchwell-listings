f4c9af12cbbbe01e7d0e96c3870e87b3a13d42b3
Keenan Johnson <keenan.johnson1@outlook.com>
2022-02-07 10:58:03 -0500

    Add Location interface

---
+++ src/app/interfaces/location.ts
@@ -0,0 +1,10 @@
+export interface Location {
+    address:           string;
+    address_with_unit: string;
+    city:              string;
+    coordinates:       number[];
+    place:             string;
+    state:             string;
+    zip:               string;
+    cross_streets:     string[];
+}
