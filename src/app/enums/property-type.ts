4668b2ecabbd1a2746928bd311744df632131224
Keenan Johnson <keenan.johnson1@outlook.com>
2022-02-07 10:26:10 -0500

    Add PropertyType enum

---
+++ src/app/enums/property-type.ts
@@ -0,0 +1,8 @@
+export enum PropertyType {
+    CoOp = "Co-op",
+    Condo = "Condo",
+    Condop = "Condop",
+    MultiFamilyTownhouse = "Multi-Family Townhouse",
+    Rental = "Rental",
+    SingleFamilyTownhouse = "Single-Family Townhouse",
+}
