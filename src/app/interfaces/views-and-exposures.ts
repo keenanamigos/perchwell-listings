246c772adbc0093d038bcf4b7e6a7c48574d00a5
Keenan Johnson <keenan.johnson1@outlook.com>
2022-02-07 10:22:10 -0500

    Add ViewsAndExposures interface

---
+++ src/app/interfaces/views-and-exposures.ts
@@ -0,0 +1,8 @@
+export interface ViewsAndExposures {
+    water_view?: boolean;
+    skyline_view?: boolean;
+    park_view?: boolean;
+    central_park_view?: boolean;
+    south_exposure?: boolean;
+    north_exposure?: boolean;
+}
