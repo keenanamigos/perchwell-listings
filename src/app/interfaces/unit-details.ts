8214ba677506b76e3f13c6f01cf2f8124005908e
Keenan Johnson <keenan.johnson1@outlook.com>
2022-02-07 10:43:30 -0500

    Add UnitDetails interface

---
+++ src/app/interfaces/unit-details.ts
@@ -0,0 +1,21 @@
+export interface UnitDetails {
+    beds:                number;
+    floor_number?:       number;
+    full_baths:          number;
+    furnished?:          Furnished;
+    general_condition?:  GeneralCondition;
+    half_baths:          number;
+    home_offices?:       number;
+    maint_cc:            number;
+    property_type:       PropertyType;
+    real_estate_tax:     number;
+    total_monthlies:     number;
+    sqft?:               number;
+    total_rooms?:        number;
+    unit_number?:        string;
+    width:               string;
+    configuration?:      Configuration;
+    max_financing_pct?:  number;
+    pct_tax_deductable?: number;
+    exterior_sqft?:      number;
+}
